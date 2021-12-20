e21ca121481cd5fab57ec5a933b887073a3f1efd
kayac-chang <egg734631@gmail.com>
2021-12-20 10:58:12 +0800

    feat: 🎸 refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,28 @@
 import clsx from "clsx";
 import { lazy, Suspense, useEffect, useState, useTransition } from "react";
 import { Asset } from "@/models";
+import API from "@/services";
 
-const NFTCard = lazy(() => import("@/components/molecules/NFTCard"));
+const NFTCard = lazy(() => import("@/components/features/NFTCard"));
 
-function getAssetBy({ tokenID, address }: Pick<Asset, "tokenID" | "address">) {
-  return fetch(
-    `https://api.opensea.io/api/v1/asset/${address}/${tokenID}/`
-  ).then((res) => res.json());
-}
-
-function App() {
+function useAsset({ address, tokenID }: Pick<Asset, "address" | "tokenID">) {
   const [asset, setAsset] = useState<Asset>();
   const [, startTransition] = useTransition();
 
   useEffect(() => {
     startTransition(() => {
-      getAssetBy({
-        address: "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb",
-        tokenID: "8712",
-      }).then(
-        ({
-          token_id,
-          asset_contract,
-          name,
-          description,
-          image_preview_url,
-          creator,
-        }) =>
-          setAsset({
-            name,
-            description: description || "",
-            tokenID: token_id,
-            address: asset_contract.address,
-            image: image_preview_url,
-            creator: {
-              name: creator.user || "anonymous",
-              image: creator.profile_img_url,
-            },
-          })
-      );
+      API.OpenSea.getAssetBy({ address, tokenID }).then(setAsset);
     });
-  }, [startTransition]);
+  }, [startTransition, address, tokenID]);
+
+  return { data: asset };
+}
+
+function App() {
+  const { data } = useAsset({
+    address: "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb",
+    tokenID: "8712",
+  });
 
   return (
     <main
@@ -51,9 +32,7 @@ function App() {
         "flex flex-col justify-center items-center"
       )}
     >
-      <Suspense fallback={"loading"}>
-        {asset && <NFTCard {...asset} />}
-      </Suspense>
+      <Suspense fallback={"loading"}>{data && <NFTCard {...data} />}</Suspense>
     </main>
   );
 }
